c5f974eafa27bfcc7a79b0edc0ca39f683d48234
fzampa <fzampa@linux-zampa>
2021-03-13 19:30:27 +0100

    Initial commit - created project

---
+++ science/src/main/java/br/com/lab/ScienceApplication.java
@@ -0,0 +1,13 @@
+package br.com.lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScienceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScienceApplication.class, args);
+	}
+
+}
